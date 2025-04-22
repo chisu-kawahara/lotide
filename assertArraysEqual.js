3a8124c57cea6d2bca316cd57e3a54131854f12c
Chisu Kawahara <chisukawahara@gmail.com>
2025-04-21 18:02:30 -0700

    initial commit

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+assertEqual ("LighthouseLabs", "Bootcamp");
+assertEqual ("LighthouseLabs", "LighthouseLabs");
+assertEqual (1, 1);
+assertEqual (1, 2);
+
+//Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+assertArraysEqual([1, 2, 3], [1, 2]);
+

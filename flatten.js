99aab2b27822bcc502899fb0f27940ba427622ac
Chisu Kawahara <chisukawahara@gmail.com>
2025-04-21 18:11:39 -0700

    initial commit

---
+++ flatten.js
@@ -0,0 +1,44 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+assertArraysEqual([1, 2, 3], [1, 2]);
+
+const eqArrays = function (arr1, arr2) {
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+	for (let i = 0; i < arr1.length; i++) {
+		if (arr1[i] !== arr2[i]) {
+			return false;
+		}
+	}
+	return true;
+};
+eqArrays([1, 2, 3], [1, 2, 3]);
+eqArrays([1, 2, 3], [3, 2, 1]);
+eqArrays(["1", "2", "3"], ["1", "2", "3"]);
+eqArrays(["1", "2", "3"], ["1", "2", 3]);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+const flatten = function(arr) {
+  let flatArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        flatArray.push(arr[i][j]);
+      }
+    } else {
+      flatArray.push(arr[i]);
+    }
+  }
+  return flatArray;
+}
+flatten([1, 2, [3, 4], 5, [6]]);

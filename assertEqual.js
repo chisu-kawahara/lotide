e47ca75299b73dae78f1cc0f8e9593046f84e80d
Chisu Kawahara <chisukawahara@gmail.com>
2025-04-26 16:37:18 -0700

    First commit

--- assertEqual.js
+++ assertEqual.js
@@ -1,19 +1,11 @@
+const assertEqual = function (actual, expected) {
+	if (actual === expected) {
+		console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+	} else {
+		console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+	}
+};
 
-
-
-/*
-Make the function compare the two values it takes in and print out a message telling us if they match or not.
-If the values match, it should print (console.log) the following: Assertion Passed: [actual] === [expected] (but with the values filled in)
-Otherwise it should print (console.log) the following: Assertion Failed: [actual] !== [expected] (but with the values filled in)
-*/
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
-assertEqual ("LighthouseLabs", "Bootcamp");
-assertEqual ("LighthouseLabs", "LighthouseLabs");
-assertEqual (1, 1);
-assertEqual (1, 2);
+// TEST CODE for different scenario
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
---
+++ countOnly.js
@@ -0,0 +1,40 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+assertEqual ("LighthouseLabs", "Bootcamp");
+assertEqual ("LighthouseLabs", "LighthouseLabs");
+assertEqual (1, 1);
+assertEqual (1, 2);
+
+const countOnly = function (allItems, itemToCount) {
+  
+  
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);

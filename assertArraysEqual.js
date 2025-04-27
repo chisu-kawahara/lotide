8d88e0d41fcd812e311d65a5a74d57b598b5ef83
Chisu Kawahara <chisukawahara@gmail.com>
2025-04-27 10:56:25 -0700

    Update assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,23 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
-assertEqual ("LighthouseLabs", "Bootcamp");
-assertEqual ("LighthouseLabs", "LighthouseLabs");
-assertEqual (1, 1);
-assertEqual (1, 2);
+const eqArrays = function (arr1, arr2) {
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+	for (let i = 0; i < arr1.length; i++) {
+		if (arr1[i] !== arr2[i]) {
+			return false;
+		}
+	}
+	return true;
+};
 
-//Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
+const assertArraysEqual = function (actual, expected) {
+	if (eqArrays(actual, expected)) {
+		console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+	} else {
+		console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+	}
+};
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
-assertArraysEqual([1, 2, 3], [1, 2]);
-
+assertArraysEqual(["a", "b"], ["a", "b"]);
+assertArraysEqual(["a", "b"], ["a", "c"]);

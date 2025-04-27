9c7305a009cd73237538e38d600e688c2388cb9f
Chisu Kawahara <chisukawahara@gmail.com>
2025-04-27 09:58:33 -0700

    First commitclear

--- head.js
+++ head.js
@@ -1,23 +1,17 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
-assertEqual ("LighthouseLabs", "Bootcamp");
-assertEqual ("LighthouseLabs", "LighthouseLabs");
-assertEqual (1, 1);
-assertEqual (1, 2);
+const assertEqual = function (actual, expected) {
+	if (actual === expected) {
+		console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+	} else {
+		console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+	}
+};
 
 const head = function(array) {
   if (array.length === 0) {
-    return undefined;
-  } else {
     return array[0];
   }
 }
 assertEqual(head([5, 6, 7]), 5); 
 assertEqual(head([5]), 5);
-assertEqual(head([]), undefined); 
+assertEqual(head([]), undefined); //An empty array 
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

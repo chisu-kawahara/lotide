c1a492f970abaaeb1b7b1574f42c43ebe1f36622
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-04-27 20:51:17 -0700

    “”

---
+++ findKey.js
@@ -0,0 +1,34 @@
+const assertEqual = function (actual, expected) {
+	if (actual === expected) {
+		console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+	} else {
+		console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+	}
+};
+
+const findKey = function(obj, callback) {
+  for (let key in obj) {
+    if (callback(obj[key])) {
+      return key; // Return the first key where callback is truthy
+    }
+  }
+  return undefined; // Return undefined if no matching key is found
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), "noma"); // Expected output: "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 4), undefined); // Expected output: undefined

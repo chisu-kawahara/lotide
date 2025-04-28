ced2892910d7f2d6cbd5e90153c8a07038d10906
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-04-27 20:33:40 -0700

    “”

---
+++ takeUntil.js
@@ -0,0 +1,40 @@
+const eqArrays = function (arr1, arr2) {
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+	for (let i = 0; i < arr1.length; i++) {
+		if (arr1[i] !== arr2[i]) {
+			return false;
+		}
+	}
+	return true;
+};
+
+const assertArraysEqual = function (actual, expected) {
+	if (eqArrays(actual, expected)) {
+		console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+	} else {
+		console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+	}
+};
+
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    if (callback(item)) {
+      break; // stop collecting when callback returns truthy
+    }
+    results.push(item);
+  }
+  return results;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1); 
+// Expected output: [1, 2, 5, 7, 2]
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2); 
+// Expected output: ["I've", "been", "to", "Hollywood"]

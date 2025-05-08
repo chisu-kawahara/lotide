d6f66b89ef8979bdd86903ebcd618d1c8abedf45
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-08 16:41:33 -0700

    “..”

--- assertEqual.js
+++ assertEqual.js
@@ -6,6 +6,5 @@ const assertEqual = function (actual, expected) {
 	}
 };
 
-// TEST CODE for different scenario
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+module.exports = assertEqual;

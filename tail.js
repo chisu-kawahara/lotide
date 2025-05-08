5bb75e19c905c280e73ccd8e2e8066bbc9e7b0d9
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-08 16:53:03 -0700

    “..”

--- tail.js
+++ tail.js
@@ -1,10 +1,6 @@
-const assertEqual = function (actual, expected) {
-	if (actual === expected) {
-		console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-	} else {
-		console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-	}
-};
+const assertEqual = require("./assertEqual");
+
+
 const tail = function (array) {
 	return array.slice(1);
 };

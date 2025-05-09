ad3002fecda607bdd2caff87ad6c9501a7912135
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-08 21:42:08 -0700

    “..”

--- test/tailTest.js
+++ test/tailTest.js
@@ -7,5 +7,3 @@ const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-console.log(tail([1, 2, 3, 4]));
-console.log(tail(["a", "b", "c"]));

f22b773fcc2886ec67d1da7dfae64e8f73198bcf
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-08 22:43:29 -0700

    “..”

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,20 @@
 const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
 
-// TEST CODE
+/*
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
 assertArraysEqual(middle([1, 2, 3]), [2]);
 assertArraysEqual(middle([1]), []);
 assertArraysEqual(middle([1, 2]), []);
 assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+*/
+
+describe("#middle", () => {
+	it("returns 1 for [1, 2, 3]", () => {
+		assert.strictEqual(middle([1, 2, 3, 4]), [2, 3]);
+	});
+	it("returns '5' for ['5']", () => {
+		assert.strictEqual(middle([1, 2, 3]), [2]);;
+	});
+});
+
--- test/tailTest.js
+++ test/tailTest.js
@@ -7,3 +7,13 @@ const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+describe("#tail", () => {
+	it("returns 1 for [1, 2, 3]", () => {
+		assert.strictEqual(tail["Hello", "Lighthouse", "Labs"]);
+	});
+	it("returns '5' for ['5']", () => {
+		assert.strictEqual(tail([1, 2, 3]), [2]);;
+	});
+});
+

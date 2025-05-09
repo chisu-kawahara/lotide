2e80edc348aacb00741ad8ddd610353018484ff5
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-08 21:47:49 -0700

    “..”

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,15 +1,6 @@
+const assertEqual = require("../assertEqual");
 
-const eqArrays = function (arr1, arr2) {
-	if (arr1.length !== arr2.length) {
-		return false;
-	}
-	for (let i = 0; i < arr1.length; i++) {
-		if (arr1[i] !== arr2[i]) {
-			return false;
-		}
-	}
-	return true;
-};
+
 eqArrays([1, 2, 3], [1, 2, 3]);
 eqArrays([1, 2, 3], [3, 2, 1]);
 eqArrays(["1", "2", "3"], ["1", "2", "3"]);
@@ -18,5 +9,3 @@ eqArrays(["1", "2", "3"], ["1", "2", 3]);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-
-module.exports = eqArrays;

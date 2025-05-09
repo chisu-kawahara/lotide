4e6b78677c6e1dd586329ae9a184126b6cc5f3c2
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-08 22:00:44 -0700

    “..”

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,3 @@
-const eqArrays = function (arr1, arr2) {
-	if (arr1.length !== arr2.length) {
-		return false;
-	}
-	for (let i = 0; i < arr1.length; i++) {
-		if (arr1[i] !== arr2[i]) {
-			return false;
-		}
-	}
-	return true;
-};
 
 const assertArraysEqual = function (actual, expected) {
 	if (eqArrays(actual, expected)) {
@@ -18,5 +7,5 @@ const assertArraysEqual = function (actual, expected) {
 	}
 };
 
-
+const eqArrays = require("../eqArrays");
 module.exports = assertArraysEqual;

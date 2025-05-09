8cdb6c005a90ea696ef2eb58eb43dc7e0be3dd0f
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-08 22:27:57 -0700

    “..”

--- test/headTest.js
+++ test/headTest.js
@@ -3,8 +3,21 @@ const assertEqual = require("../head");
 const head = function (array) {
 	return array[0];
 };
-
+/*
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head([5]), 5);
 assertEqual(head([]), undefined); //An empty array
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+*/
+
+const assert = require('chai').assert;
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
+
+it("returns '5' for ['5']", () => {
+	assert.strictEqual(head(['5']), '5'); 
+});

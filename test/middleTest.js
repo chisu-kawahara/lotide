9a6058597ac21dfd6a3689d27b96787737c01b8d
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-08 22:58:26 -0700

    “..”

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,4 @@
-const assertArraysEqual = require("../assertArraysEqual");
+//const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
 const assert = require('chai').assert;
 
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,13 @@
-const assertEqual = require("../assertEqual");
+//const assertEqual = require("../assertEqual");
 const tail = require('../tail');
 const assert = require('chai').assert;
 
 
 //TEST CODE
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+assert.strictEqual(result.length, 2); // ensure we get back two elements
+assert.strictEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assert.strictEqual(result[1], "Labs"); // ensure second element is "Labs"
 
 describe("#tail", () => {
   it("returns the correct tail of the array", () => {

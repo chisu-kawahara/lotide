6d9050e73f6766c12e269253e7bfb22af06640e1
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-08 21:30:19 -0700

    “..”

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,5 @@
-const assertEqual = function (actual, expected) {
-	if (actual === expected) {
-		console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-	} else {
-		console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-	}
-};
+const assertEqual = require("./assertEqual");
+
 
 const tail = function (array) {
 	return array.slice(1);

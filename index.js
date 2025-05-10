c5acfa13ddf909eb28332217bf05e052f95ebecd
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-09 21:19:13 -0700

    “”

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const assertEqual = require('./assertEqual');
+const assertArrayEqual = require('./assertArraysEqual');
+const assertObjectEqual = require('./assertObjectsEqual');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
 
 module.exports = {
   head:   head,
   tail:   tail,
-  middle: middle
+  middle: middle,
+  assertEqual: assertEqual,
+  assertArrayEqual: assertArrayEqual,
+  assertObjectEqual: assertObjectEqual,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions
+
 };
 
 

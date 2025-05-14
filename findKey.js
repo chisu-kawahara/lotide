c2260766084688f41c655f093810c5b928651c31
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-13 19:45:04 -0700

    “updated”

--- findKey.js
+++ findKey.js
@@ -1,5 +1,9 @@
 const assertEqual = require("./assertEqual");
 
+
+// The findKey function takes an object and a callback function as arguments.
+// It iterates through the object's keys and values, applying the callback to each value.
+// If the callback returns a truthy value for a value, it returns the corresponding key.
 const findKey = function(obj, callback) {
   for (let key in obj) {
     if (callback(obj[key])) {

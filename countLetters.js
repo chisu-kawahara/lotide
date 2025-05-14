bb59e4d4dd7fb018bbb82c3e6a37090d3bbb844c
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-14 13:08:41 -0700

    Add countLetters function to count non-space characters in a string

--- countLetters.js
+++ countLetters.js
@@ -17,9 +17,4 @@ const countLetters = function (str) {
   return letterCounts;
 };
 
-// TEST CODE
-assertEqual(countLetters("hello").h, 1); // h: 1
-assertEqual(countLetters("hello").e, 1); // e: 1 
-assertEqual(countLetters("hello").l, 2); // l: 2
-
 module.exports = countLetters;

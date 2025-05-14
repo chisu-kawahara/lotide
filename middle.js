161b59e89ccd3267ebae67ce2a40e70c42fc80e8
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-14 13:22:22 -0700

    Add middle function to return middle element(s) of an array

--- middle.js
+++ middle.js
@@ -1,4 +1,10 @@
 
+//middle function to find the middle element(s) of an array
+// The function takes an array as input and returns the middle element(s) of the array.
+// If the array has an odd number of elements, it returns the middle element.
+// If the array has an even number of elements, it returns the two middle elements.
+// The function first checks if the array has less than 3 elements.
+// If so, it returns an empty array.
 
 const middle = function (arr) {
 	// Return an empty array if less than 3 elements
--- test/middleTest.js
+++ test/middleTest.js
@@ -3,7 +3,7 @@ const assertArraysEqual = require("../assertArraysEqual");
 
 const assert = require('chai').assert;
 
-//TEST CODE
+//TEST cases
 describe("#middle", () => {
   it("returns 1 for [1, 2, 3]", () => {
     assert.deepEqual(middle([1, 2, 3]), [2]);

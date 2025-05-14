191964b76f45d9419d5a86f47b2d3cfa595f4965
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-13 18:52:48 -0700

    “updated”

--- countLetters.js
+++ countLetters.js
@@ -2,22 +2,26 @@ const assertEqual = require("./assertEqual");
 
 
 const countLetters = function (str) {
-	const letterCounts = {};
+  const letterCounts = {};
 
-	for (const letter of str) {
-		if (letter !== ' ') {
-			// existing code
-		}
-	}
+  for (const letter of str) {
+    if (letter !== ' ') {
+      if (letterCounts[letter]) {
+        letterCounts[letter] += 1;
+      } else {
+        letterCounts[letter] = 1;
+      }
+    }
+  }
 
-	for (const letter of str) {
-		if (letterCounts[letter]) {
-			letterCounts[letter] += 1;
-		} else {
-			letterCounts[letter] = 1;
-		}
-	}
-	return letterCounts;
+  return letterCounts;
 };
+
+// TEST CODE
+assertEqual(countLetters("hello").h, 1); // h: 1
+assertEqual(countLetters("hello").e, 1); // e: 1
+assertEqual(countLetters("hello").l, 2); // l: 2
+assertEqual(countLetters("hello").o, 1); // o: 1
+assertEqual(countLetters("lighthouse").l, 1); // l: 1
 
 module.exports = countLetters;
--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -1,9 +1,8 @@
 const countLetters = require('../countLetters');
 
 // TEST CODE
-console.log(countLetters("hello")); // Output: { h: 1, e: 1, l: 2, o: 1 }
-console.log(countLetters("lighthouse")); // Output: { l: 1, i: 1, g: 1, h: 1, t: 1, o: 1, u: 1, s: 1, e: 1 }
-
-const result = countLetters('LHL');
-assertEqual(result['L'], 2);
-assertEqual(result['H'], 1);
+assertEqual(countLetters("hello").h, 1); // h: 1
+assertEqual(countLetters("hello").e, 1); // e: 1
+assertEqual(countLetters("hello").l, 2); // l: 2
+assertEqual(countLetters("hello").o, 1); // o: 1
+assertEqual(countLetters("lighthouse").l, 1); // l: 1

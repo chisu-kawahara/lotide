deef0847d4a6a449484a6bdb893cf6ed88f89e60
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-13 17:59:36 -0700

    “updated”

--- flatten.js
+++ flatten.js
@@ -1,30 +1,6 @@
-const assertArraysEqual = function (actual, expected) {
-	if (eqArrays(actual, expected)) {
-		console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-	} else {
-		console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-	}
-};
-
-const assertEqual = function (actual, expected) {
-	if (actual === expected) {
-		console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-	} else {
-		console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-	}
-};
-
-const eqArrays = function (arr1, arr2) {
-	if (arr1.length !== arr2.length) {
-		return false;
-	}
-	for (let i = 0; i < arr1.length; i++) {
-		if (arr1[i] !== arr2[i]) {
-			return false;
-		}
-	}
-	return true;
-};
+const eqArrays = require("./eqArrays");
+const assertEqual = require("./assertEqual");
+const assertArraysEqual = require("./assertArraysEqual");
 
 const flatten = function (arr) {
 	let result = [];

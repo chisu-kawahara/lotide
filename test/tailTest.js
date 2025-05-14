ff285ed30ba33108683d1329fc021efa0df399c2
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-13 20:31:12 -0700

    “updated”

--- test/tailTest.js
+++ test/tailTest.js
@@ -22,7 +22,6 @@ assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 assertEqual(result[2], undefined); // ensure third element is undefined
-assertEqual(result[3], undefined); // ensure fourth element is undefined
 
 const words = ["Hello", "Lighthouse", "Labs"];
 tail(words);

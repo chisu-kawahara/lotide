23129f06d5acadca0acbd69d26c918864ad7c8b9
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-13 20:00:04 -0700

    “updated”

--- letterPositions.js
+++ letterPositions.js
@@ -1,22 +1,6 @@
-const eqArrays = function (arr1, arr2) {
-	if (arr1.length !== arr2.length) {
-		return false;
-	}
-	for (let i = 0; i < arr1.length; i++) {
-		if (arr1[i] !== arr2[i]) {
-			return false;
-		}
-	}
-	return true;
-};
+const eqArrays = require ("./eqArrays");
+const assertArraysEqual = require ("./assertArraysEqual");
 
-const assertArraysEqual = function (actual, expected) {
-	if (eqArrays(actual, expected)) {
-		console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-	} else {
-		console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-	}
-};
 
 const letterPositions = function (str) {
 	const positions = {};
@@ -37,11 +21,5 @@ const letterPositions = function (str) {
 	return positions;
 };
 
-const results = letterPositions("hello");
-assertArraysEqual(results.e, [1]); // 'e' is at index 1
-// You can add more test cases for other letters in "hello"
-assertArraysEqual(results.h, [0]); // 'h' is at index 0
-assertArraysEqual(results.l, [2, 3]); // 'l' is at indices 2 and 3
-assertArraysEqual(results.o, [4]); // 'o' is at index 4
 
 module.exports = letterPositions;

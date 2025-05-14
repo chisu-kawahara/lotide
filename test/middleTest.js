2ac7b7a6fa764d1d4b7e94b125114584fdc43eea
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-13 20:41:18 -0700

    “updated”

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,21 @@
 const middle = require("../middle");
 const assertArraysEqual = require("../assertArraysEqual");
 
+const assert = require('chai').assert;
+const middle   = require('../middle');
 
+describe("#middle", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
+
+//Test cases 
+assert.strictEqual(middle([1, 2, 3]), [2]);
+assert.strictEqual(middle([1, 2, 3, 4]), [2, 3]);
+assert.strictEqual(middle([1, 2]), []);
+assert.strictEqual(middle([1]), []);
+assert.strictEqual(middle([1, 2, 3]), [2]);
 
 //Test cases
 assertArraysEqual(middle([1, 2, 3]), [2]);

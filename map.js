25755beb4d9dfb6c4a3bee4ef55aa67764641c12
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-13 20:07:02 -0700

    “updated”

--- map.js
+++ map.js
@@ -1,23 +1,5 @@
-
-const eqArrays = function (arr1, arr2) {
-	if (arr1.length !== arr2.length) {
-		return false;
-	}
-	for (let i = 0; i < arr1.length; i++) {
-		if (arr1[i] !== arr2[i]) {
-			return false;
-		}
-	}
-	return true;
-};
-
-const assertArraysEqual = function (actual, expected) {
-	if (eqArrays(actual, expected)) {
-		console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-	} else {
-		console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-	}
-};
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
 const map = function (array, callback) {
   const results = [];
---
+++ test/mapTest.js
@@ -0,0 +1,11 @@
+const map = require('../map.js');
+const assertArraysEqual = require('../assertArraysEqual.js');
+
+//Test cases
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+const results2 = map(words, word => word.length);
+assertArraysEqual(results2, [6, 7, 2, 5, 3]);
+const results3 = map(words, word => word.toUpperCase());
+assertArraysEqual(results3, ['GROUND', 'CONTROL', 'TO', 'MAJOR', 'TOM']);

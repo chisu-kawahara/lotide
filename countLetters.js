3b437d5561ef737b9967e178ed1ccf77825c7163
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-13 18:54:27 -0700

    “updated”

--- countLetters.js
+++ countLetters.js
@@ -23,5 +23,6 @@ assertEqual(countLetters("hello").e, 1); // e: 1
 assertEqual(countLetters("hello").l, 2); // l: 2
 assertEqual(countLetters("hello").o, 1); // o: 1
 assertEqual(countLetters("lighthouse").l, 1); // l: 1
+assertEqual(countLetters("").h, undefined); // empty string should not have any letters, undefined for h
 
 module.exports = countLetters;

36f4cedbbde345e24508278d645898e77287d55c
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-13 21:14:59 -0700

    “updated”

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -3,7 +3,12 @@ const assertArrayEqual = require("../assertArraysEqual");
 
 
 //TEST CODE
-const words = ["hello", "world", "lighthouse"];
 assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
 assertArraysEqual(without(words, ["lighthouse"]), ["hello", "world"]);
+// Make sure the original array is not modified
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+// Test with an empty array
+assertArraysEqual(without([], [1]), []);
+// Test with an empty itemsToRemove array
+assertArraysEqual(without([1, 2, 3], []), [1, 2, 3]);

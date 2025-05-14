a5393f67aea1943677464351da2c4cfa630eb42e
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-13 20:57:38 -0700

    “updated”

--- test/tailTest.js
+++ test/tailTest.js
@@ -11,22 +11,13 @@ assert.strictEqual(result[1], "Labs"); // ensure second element is "Labs"
 describe("#tail", () => {
   it("returns the correct tail of the array", () => {
     const result = tail(["Hello", "Lighthouse", "Labs"]);
-    assertStrictEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2); // ensure we get back two elements
-    assertStrictEqual(tail(["Hello", "Lighthouse", "Labs"])[0], "Lighthouse"); // ensure first element is "Lighthouse"
-    assertStrictEqual(tail(["Hello", "Lighthouse", "Labs"])[1], "Labs"); // ensure second element is "Labs"
-    assertEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2); // ensure we get back two elements
-  });
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2); // ensure we get back two elements
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[0], "Lighthouse"); // ensure first element is "Lighthouse"
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[1], "Labs"); // ensure second element is "Labs"
+    assert.strictEqual(tail(["test"]).length, 0);
+    assert.strictEqual(tail([]).length, 0);
+    assert.strictEqual(tail([1, 2, 3]).length, 2); // ensure we get back two elements
+    assert.strictEqual(tail([1, 2, 3])[0], 2); // ensure first element is 2
+    assert.strictEqual(tail([1, 2, 3])[1], 3); // ensure second element is 3
 });
-
-const words = ["Hello", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3); // ensure original array is unchanged
-assertEqual(words[0], "Hello"); // ensure first element is "Hello"
-assertEqual(words[1], "Lighthouse"); // ensure second element is "Lighthouse"
-assertEqual(words[2], "Labs"); // ensure third element is "Labs"
-
-assertEqual(tail(["test"]).length, 0);
-assertEqual(tail([]).length, 0);
-assertEqual(tail([1, 2, 3]).length, 2); // ensure we get back two elements
-assertEqual(tail([1, 2, 3])[0], 2); // ensure first element is 2
-assertEqual(tail([1, 2, 3])[1], 3); // ensure second element is 3
+});

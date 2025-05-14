b0b8f56a12e325bf3b02790f91a24087a4c01abb
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-13 18:47:32 -0700

    “updated”

--- countLetters.js
+++ countLetters.js
@@ -1,10 +1,5 @@
-const assertEqual = function (actual, expected) {
-	if (actual === expected) {
-		console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-	} else {
-		console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-	}
-};
+const assertEqual = require("./assertEqual");
+
 
 const countLetters = function (str) {
 	const letterCounts = {};
@@ -19,8 +14,6 @@ const countLetters = function (str) {
 	return letterCounts;
 };
 
-// TEST CODE
-console.log(countLetters("hello")); // Output: { h: 1, e: 1, l: 2, o: 1 }
-console.log(countLetters("lighthouse")); // Output: { l: 1, i: 1, g: 1, h: 1, t: 1, o: 1, u: 1, s: 1, e: 1 }
+
 
 module.exports = countLetters;

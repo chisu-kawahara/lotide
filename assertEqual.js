1552d5eac50b7fa105c7f82022b6e1784d4df747
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-13 21:52:50 -0700

    add test cases for assertEqual function

--- assertEqual.js
+++ assertEqual.js
@@ -6,5 +6,13 @@ const assertEqual = function (actual, expected) {
 	}
 };
 
+// Test cases
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual("1", 1);
+assertEqual("1", "1");
+assertEqual("1", 1);
+
 
 module.exports = assertEqual;

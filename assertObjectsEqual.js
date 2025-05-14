f7f015fbb41b823fbbdedad0962e4acb8d9bde71
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-13 17:55:05 -0700

    “updated”

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,34 +1,18 @@
 const eqArrays = require("./eqArrays");
 const eqObjects = require("./eqObjects");
+const util = require("util");
 
-const assertObjectsEqual = function (actual, expected) {
-	const inspect = require("util").inspect;
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = util.inspect;
 
-	const eqObjects = function (object1, object2) {
-		const keys1 = Object.keys(object1);
-		const keys2 = Object.keys(object2);
-		if (keys1.length !== keys2.length) {
-			return false;
-		}
-		for (const key of keys1) {
-			const val1 = object1[key];
-			const val2 = object2[key];
-
-			if (Array.isArray(val1) && Array.isArray(val2)) {
-				if (!eqArrays(val1, val2)) {
-					return false;
-				}
-			} else {
-				if (val1 !== val2) {
-					return false;
-				}
-			}
-		}
-	};
-	return true;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
 };
 
-// Test
-assertObjectsEqual({ a: "1", b: 2 }, { b: 2, a: "1" });
-assertObjectsEqual({ a: "1", b: 2 }, { a: "1", b: 3 });
-assertObjectsEqual({ a: "1", b: "2" }, { a: "1", b: 2 });
+// Test cases
+assertObjectsEqual({ a: "1", b: 2 }, { b: 2, a: "1" }); // ✅ Assertion Passed
+assertObjectsEqual({ a: "1", b: 2 }, { a: "1", b: 3 }); // 🛑 Assertion Failed
+assertObjectsEqual({ a: "1", b: "2" }, { a: "1", b: 2 }); // 🛑 Assertion Failed

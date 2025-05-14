7310ebfdb519d83e15222720cc208543873a3cd8
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-14 13:03:09 -0700

    add test cases for assertObjectEqual function

--- test/assertObjectsEqualTest.js
+++ test/assertObjectsEqualTest.js
@@ -6,3 +6,4 @@ assertObjectsEqual({ a: "1", b: 2 }, { a: "1", b: 3 }); // 🛑 Assertion Failed
 assertObjectsEqual({ a: "1", b: "2" }, { a: "1", b: 2 }); // 🛑 Assertion Failed
 assertObjectsEqual({ a: "1", b: 2 }, { a: "1", b: 2 }); // ✅ Assertion Passed
 assertObjectsEqual({ a: "1", b: 2 }, { a: "1", b: 2, c: 3 }); // 🛑 Assertion Failed
+assertObjectsEqual({ a: "1", b: 2, c: 3 }, { a: "1", b: 2 }); // 🛑 Assertion Failed

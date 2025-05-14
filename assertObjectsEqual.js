c9f863403884c5b3db3a6c6cb68cd06fe68fea9b
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-13 18:40:04 -0700

    “updated”

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -12,9 +12,5 @@ const assertObjectsEqual = function(actual, expected) {
   }
 };
 
-// Test cases
-assertObjectsEqual({ a: "1", b: 2 }, { b: 2, a: "1" }); // ✅ Assertion Passed
-assertObjectsEqual({ a: "1", b: 2 }, { a: "1", b: 3 }); // 🛑 Assertion Failed
-assertObjectsEqual({ a: "1", b: "2" }, { a: "1", b: 2 }); // 🛑 Assertion Failed
 
 module.exports = assertObjectsEqual;
---
+++ test/assertObjectsEqualTest.js
@@ -0,0 +1,6 @@
+const assertObjectsEqual = require("../assertObjectsEqual");
+
+// Test cases
+assertObjectsEqual({ a: "1", b: 2 }, { b: 2, a: "1" }); // ✅ Assertion Passed
+assertObjectsEqual({ a: "1", b: 2 }, { a: "1", b: 3 }); // 🛑 Assertion Failed
+assertObjectsEqual({ a: "1", b: "2" }, { a: "1", b: 2 }); // 🛑 Assertion Failed
